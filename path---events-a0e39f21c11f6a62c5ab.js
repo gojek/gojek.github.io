30702d7a1ae811f64b9a2bd339634a470efe7e04
Shilpa <shilpa@zysk.in>
2018-09-03 17:25:33 +0530

    Added new JD

--- path---events-a0e39f21c11f6a62c5ab.js
+++ path---events-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([44713349584700],{471:function(t,n){t.exports={pathContext:{}}}});
+webpackJsonp([44713349584700],{473:function(t,n){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---events-a0e39f21c11f6a62c5ab.js.map
--- path---go-viet-a0e39f21c11f6a62c5ab.js
+++ path---go-viet-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([90905148958094],{472:function(t,n){t.exports={pathContext:{}}}});
+webpackJsonp([90905148958094],{474:function(t,n){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---go-viet-a0e39f21c11f6a62c5ab.js.map
