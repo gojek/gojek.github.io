169a84b6ba94dd9357adf11f4e841db0846990f4
varunnotkar59 <shilpa@zysk.in>
2018-11-28 23:48:52 +0530

    Fix - Google analytics code check

--- src/html.js
+++ src/html.js
@@ -31,6 +31,14 @@ module.exports = class HTML extends React.Component {
           />
           {this.props.headComponents}
           {css}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-130067050-1"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag() { dataLayer.push(arguments); }
+            gtag('js', new Date());
+
+            gtag('config', 'UA-130067050-1');
+          </script>
         </head>
         <body {...this.props.bodyAttributes}>
           {/* test */}
