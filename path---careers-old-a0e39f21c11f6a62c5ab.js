ae3e4029233cf92ba9f6945919427594d8d24792
Charan <charan.r@pacewisdom.com>
2019-07-12 21:31:21 +0530

    Added news article

--- path---careers-old-a0e39f21c11f6a62c5ab.js
+++ path---careers-old-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([27199168719485],{601:function(t,n){t.exports={pathContext:{}}}});
+webpackJsonp([27199168719485],{603:function(t,n){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---careers-old-a0e39f21c11f6a62c5ab.js.map
---
+++ path---description-a0e39f21c11f6a62c5ab.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x5b20b9c27a61],{620:function(t,n){t.exports={pathContext:{}}}});
+//# sourceMappingURL=path---description-a0e39f21c11f6a62c5ab.js.map
