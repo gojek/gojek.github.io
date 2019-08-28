3f97e0f9ad8e8b17c528cc9084c292f9845753cc
Charan <charan.r@pacewisdom.com>
2019-08-28 21:14:18 +0530

    added about-us page footer

--- path---android-dev-go-food-a0e39f21c11f6a62c5ab.js
+++ path---android-dev-go-food-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xdd70e889a68d],{541:function(t,e){t.exports={pathContext:{}}}});
+webpackJsonp([0xdd70e889a68d],{538:function(t,e){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---android-dev-go-food-a0e39f21c11f6a62c5ab.js.map
--- path---gomakeimpact-a0e39f21c11f6a62c5ab.js
+++ path---gomakeimpact-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xeb33b5573654],{551:function(t,e){t.exports={pathContext:{}}}});
+webpackJsonp([0xeb33b5573654],{548:function(t,e){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---gomakeimpact-a0e39f21c11f6a62c5ab.js.map
